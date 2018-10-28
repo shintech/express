ae9de881343fae14afd91bb4fa6a011327c90eb3
Michael Prather <mprather1@gmail.com>
2018-10-28 03:31:38 +0000

    shintech-logger

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const environment = process.env['NODE_ENV']
 const port = process.env['PORT'] || 8000
 const host = process.env['HOST'] || 'localhost'
 
-const logger = require('./server/logger')({ environment })
+const logger = require('shintech-logger')({ environment })
 const server = require('./server')({ logger, environment, port })
 const pkg = require('./package.json')
 
